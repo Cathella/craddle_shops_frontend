5008a7bf2691213414ef73ada13f51b697949fda
Cathella <ncathie214@gmail.com>
2022-02-01 15:35:44 +0300

    Setup eslint to understand jsx

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import ReactDOM from "react-dom";
 import Shop from "./Shop";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Craddle Shops"),
-    React.createElement(Shop, {
-      name: "Tami's collection",
-      category: "Kid's Clothes",
-      location: "Kireka - Today",
-    }),
-    React.createElement(Shop, {
-      name: "Two steps",
-      category: "Kid's Clothes",
-      location: "Namugongo",
-    })
-  );
-};
+  return (
+    <div>
+      <h1>Craddle Shops</h1>
+      <Shop name="Tami's collection" category="Kid's wear" location="Namugongo" />
+      <Shop name="Two Steps" category="Kid's foot wear" location="Kireka" />
+    </div>
+  )
+}
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Shop.js
+++ src/Shop.js
@@ -1,11 +1,11 @@
-import React from "react";
-
 const Shop = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.category),
-    React.createElement("h3", {}, props.location),
-  ]);
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.category}</h3>
+      <h3>{props.location}</h3>
+    </div>
+  );
 };
 
 export default Shop;
