235ad05085b2f9aa002071ded48f245bcebe4b7e
Cathella <ncathie214@gmail.com>
2022-02-01 06:51:58 +0300

    Use props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+const Shop = (props) => {
+  return React.createElement(
+    "div",
+    {},
+    [
+      React.createElement("h2", {}, props.name),
+      React.createElement("h3", {}, props.category),
+      React.createElement("h3", {}, props.location),
+    ]
+  )
+};
 
-    const App = () => {
-      return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {}, "My Shop")
-      );
-    };
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Craddle Shops"),
+    React.createElement(Shop, { name: "Tami's collection", category: "Kid's Clothes", location: "Kireka"}),
+    React.createElement(Shop, { name: "Next steps", category: "Kid's Clothes", location: "Namugongo"}),
+  );
+};
 
-    ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
